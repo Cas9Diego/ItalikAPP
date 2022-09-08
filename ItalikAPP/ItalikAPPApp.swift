93a02074a8f88b7d45f00118bd23f4c451495112
Cas9Diego <DaCastro@outlook.com>
2022-09-07 19:31:38 -0500

    Initial Commit

---
+++ ItalikAPP/ItalikAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItalikAPPApp.swift
+//  ItalikAPP
+//
+//  Created by Diego Castro on 07/09/22.
+//
+
+import SwiftUI
+
+@main
+struct ItalikAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
